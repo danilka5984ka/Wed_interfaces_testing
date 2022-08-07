ac9b39099b24f43238167f59aa17c1c6d5af7e99
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-08-07 23:17:41 +0700

    Addith chromeOptions

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -19,7 +20,11 @@ public class CardOrderTest {
 
     @BeforeEach
     void setUp1() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
