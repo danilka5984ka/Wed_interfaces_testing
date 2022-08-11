60aa7db8057dc2ed901011fadf4f1d7f764c21ea
Daniil Peresumkin <danilka5984ka@yandex.ru>
2022-08-11 17:41:52 +0700

    Added checkbox testÐ

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -96,4 +96,14 @@ public class CardOrderTest {
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 
+    @Test
+    void test6() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79458795645");
+        //driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("checkbox__text")).getText();
+        Assertions.assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
 }
